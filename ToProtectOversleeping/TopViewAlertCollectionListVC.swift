051164bdea205bc5cef25f6741a90482e072de31
Hiroki Kondo <kondo.matr02@gmail.com>
2021-05-03 21:31:25 +0900

    TopViewAlertCollectionListVCを追加

---
+++ ToProtectOversleeping/TopViewAlertCollectionListVC.swift
@@ -0,0 +1,21 @@
+//
+//  TopViewAlertCollectionListVC.swift
+//  ToProtectOversleeping
+//
+//  Created by 近藤宏輝 on 2021/05/03.
+//
+
+import UIKit
+
+class TopViewAlertCollectionListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+
+    }
+    
+
+
+}
