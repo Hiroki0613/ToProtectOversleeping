99ad79c20b81234bdd24e5199fff5da809a20cfc
Hiroki Kondo <kondo.matr02@gmail.com>
2021-05-04 17:56:11 +0900

    WakeUpAndCutAlertBySlideVCを実装

---
+++ ToProtectOversleeping/Screens/WakeUpAndCutAlertBySlideVC.swift
@@ -0,0 +1,20 @@
+//
+//  WakeUpAndCutAlertBySlideVC.swift
+//  ToProtectOversleeping
+//
+//  Created by 近藤宏輝 on 2021/05/04.
+//
+
+import UIKit
+
+class WakeUpAndCutAlertBySlideVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
