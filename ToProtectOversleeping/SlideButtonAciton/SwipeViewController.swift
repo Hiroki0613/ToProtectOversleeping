97f1e79f9ae80ed41987b187e64045b613791c32
Hiroki Kondo <kondo.matr02@gmail.com>
2021-05-04 13:40:10 +0900

    SlideButtonAcitonを追加

---
+++ ToProtectOversleeping/SlideButtonAciton/SwipeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  slideButtonAciton
+//
+//  Created by 近藤宏輝 on 2021/05/02.
+//
+
+import UIKit
+
+class SwipeViewController: UIViewController {
+
+    var swipeButton: SwipeButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupSwipeButton()
+    }
+
+
+    func setupSwipeButton() {
+        if swipeButton == nil {
+            self.swipeButton = SwipeButton(frame: CGRect(x: 40, y: 200, width: 300, height: 50))
+            swipeButton.isRightToLeft = false
+            swipeButton.text = "すぐ買う"
+            swipeButton.swipedText = "購入いたしました"
+
+            self.view.addSubview(self.swipeButton)
+        }
+    }
+}
