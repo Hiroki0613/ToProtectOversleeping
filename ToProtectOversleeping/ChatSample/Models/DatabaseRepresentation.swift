2800923b76484d3d6d9ab9995a1e9637f7592642
Hiroki Kondo <kondo.matr02@gmail.com>
2021-05-17 23:52:10 +0900

    サンプルモデルを実装

---
+++ ToProtectOversleeping/ChatSample/Models/DatabaseRepresentation.swift
@@ -0,0 +1,12 @@
+//
+//  DatabaseRepresentation.swift
+//  ToProtectOversleeping
+//
+//  Created by 近藤宏輝 on 2021/05/16.
+//
+
+import Foundation
+
+protocol DatabaseRepresentation {
+  var representation: [String: Any] { get }
+}
