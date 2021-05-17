70b68c927f4cb047bd037f67e56f4d324ca1821d
Hiroki Kondo <kondo.matr02@gmail.com>
2021-05-18 00:14:22 +0900

    pod install

---
+++ ToProtectOversleeping/ChatSample/Extensions/UICollectionView+Additions.swift
@@ -0,0 +1,25 @@
+//
+//  UICollectionView+Additions.swift
+//  ToProtectOversleeping
+//
+//  Created by 近藤宏輝 on 2021/05/16.
+//
+
+import UIKit
+
+extension UIScrollView {
+  
+  // メッセージがある高さを確認
+  var isAtBottom: Bool {
+    return contentOffset.y >= verticalOffsetForBottom
+  }
+  
+  var verticalOffsetForBottom: CGFloat {
+    let scrollViewHeight = bounds.height
+    let scrollContentSizeHeight = contentSize.height
+    let bottomInset = contentInset.bottom
+    let scrollViewBottomOffset = scrollContentSizeHeight + bottomInset - scrollViewHeight
+    return scrollViewBottomOffset
+  }
+  
+}
