f9586c10907fe25742b1164a76fbf280960cefe7
Hiroki Kondo <kondo.matr02@gmail.com>
2021-05-22 21:58:39 +0900

    GPSを取得して、位置を画面上に表示

--- ToProtectOversleeping/Screens/Setting/GetGpsAddressVC.swift
+++ ToProtectOversleeping/Screens/Setting/GetGpsAddressVC.swift
@@ -13,15 +13,15 @@ import MapKit
 class GetGpsAddressVC: BaseGpsVC {
     
     // 起きる時間のカード
-//    var getGpsAddressView = GetGpsAddressView()
+    //    var getGpsAddressView = GetGpsAddressView()
     
     // 暫定で家の近くに設定している
-    var myAddressLatitude = 35.7140224101
+    var myAddressLatitude = 25.7140224101
     var myAddressLongitude = 139.65363018
     var mySettingAlarmTime = Date()
     
     var alarm = Alarm()
-
+    
     // 地図
     var mapView = MKMapView()
     var homeLocationLabel = WUBodyLabel(fontSize: 20)
@@ -32,14 +32,14 @@ class GetGpsAddressVC: BaseGpsVC {
     // アドレスを格納
     var addressString = ""
     
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemOrange
         configureView()
-//        configureDecoration()
-//        configureAddTarget()
+        //        configureDecoration()
+        //        configureAddTarget()
         // ここにdefaultで設定した住まいを入れる渡す。
         myHomeLocation = CLLocationCoordinate2D(latitude: myAddressLatitude, longitude: myAddressLongitude)
         moveTo(center: myHomeLocation, animated: false)
@@ -53,6 +53,7 @@ class GetGpsAddressVC: BaseGpsVC {
         moveTo(center: myHomeLocation, animated: false)
         self.tabBarController?.tabBar.isHidden = true
         navigationController?.setNavigationBarHidden(false, animated: true)
+        configureAddTarget()
     }
     
     func configureAddTarget() {
@@ -63,16 +64,16 @@ class GetGpsAddressVC: BaseGpsVC {
     // ここで目覚ましをセット
     @objc func setAlarmSwitch(sender: UISwitch) {
         let onCheck: Bool = sender.isOn
-            // UISwitch値を確認
-            if onCheck {
-                // viewのalphaを1.0にする。
-                // 目覚ましをONにする
-                print("スイッチの状態はオンです。値: \(onCheck)")
-            } else {
-                // viewのalphaを0.8にする。
-                // 目覚ましをOFFにする
-                print("スイッチの状態はオフです。値: \(onCheck)")
-            }
+        // UISwitch値を確認
+        if onCheck {
+            // viewのalphaを1.0にする。
+            // 目覚ましをONにする
+            print("スイッチの状態はオンです。値: \(onCheck)")
+        } else {
+            // viewのalphaを0.8にする。
+            // 目覚ましをOFFにする
+            print("スイッチの状態はオフです。値: \(onCheck)")
+        }
     }
     
     // 地図を任意の場所へ移動させる
@@ -109,22 +110,39 @@ class GetGpsAddressVC: BaseGpsVC {
         navigationController?.pushViewController(wakeUpAndCutAlertBySlideVC, animated: true)
     }
     
+    private func setAnnotation(location: CLLocationCoordinate2D) {
+        mapView.removeAnnotations(mapView.annotations)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        mapView.addAnnotation(annotation)
+    }
+    
     
     // ここでGPSを取得
     @objc func tapSetGPSButton() {
         getCurrentLocation()
         print(geoCoderLongitude)
         print(geoCoderLatitude)
-//        getGpsAddressView.prefectureAndCityNameLabel.text = address
+        
+        
+        homeLocationLabel.text = address
+        print("GpsButtonが押されました")
+        print(address)
+        let geoCoderLocation = CLLocationCoordinate2D(latitude: geoCoderLatitude, longitude: geoCoderLongitude)
+        moveTo(center: geoCoderLocation, animated: true)
+        drawCircle(center: geoCoderLocation, meter: 10, times: 10)
+        setAnnotation(location: geoCoderLocation)
+        //        getGpsAddressView.prefectureAndCityNameLabel.text = address
         
         // 情報は一時的にUserDefaultに保管する。
         
         
-//        let wakeUpAndCutAlertBySlideVC = WakeUpAndCutAlertBySlideVC()
-//        wakeUpAndCutAlertBySlideVC.myAddressLatitude = geoCoderLatitude
-//        wakeUpAndCutAlertBySlideVC.myAddressLongitude = geoCoderLongitude
-////        wakeUpAndCutAlertBySlideVC.mySettingAlarmTime = getGpsAddressView.datePicker.date
-//        navigationController?.pushViewController(wakeUpAndCutAlertBySlideVC, animated: true)
+        //        let wakeUpAndCutAlertBySlideVC = WakeUpAndCutAlertBySlideVC()
+        //        wakeUpAndCutAlertBySlideVC.myAddressLatitude = geoCoderLatitude
+        //        wakeUpAndCutAlertBySlideVC.myAddressLongitude = geoCoderLongitude
+        ////        wakeUpAndCutAlertBySlideVC.mySettingAlarmTime = getGpsAddressView.datePicker.date
+        //        navigationController?.pushViewController(wakeUpAndCutAlertBySlideVC, animated: true)
     }
     
     // 位置から住所を取得
@@ -168,7 +186,7 @@ class GetGpsAddressVC: BaseGpsVC {
         let padding: CGFloat = 40.0;
         
         NSLayoutConstraint.activate([
-                                        mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mapView.heightAnchor.constraint(equalToConstant: view.frame.size.width),
@@ -178,25 +196,25 @@ class GetGpsAddressVC: BaseGpsVC {
             homeLocationLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
             homeLocationLabel.heightAnchor.constraint(equalToConstant: padding),
             
-            homeLocationFetchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            homeLocationFetchButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
             homeLocationFetchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
             homeLocationFetchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
             homeLocationFetchButton.heightAnchor.constraint(equalToConstant: 40)
         ])
         
-//        getGpsAddressView.frame = CGRect(x: 10, y: 50, width: view.frame.size.width - 20, height: 200)
-//        view.addSubview(getGpsAddressView)
+        //        getGpsAddressView.frame = CGRect(x: 10, y: 50, width: view.frame.size.width - 20, height: 200)
+        //        view.addSubview(getGpsAddressView)
     }
     
     // セルを装飾
-//    private func configureDecoration() {
-//        getGpsAddressView.layer.shadowColor = UIColor.systemGray.cgColor
-//        getGpsAddressView.layer.cornerRadius = 16
-//        getGpsAddressView.layer.shadowOpacity = 0.1
-//        getGpsAddressView.layer.shadowRadius = 10
-//        getGpsAddressView.layer.shadowOffset = .init(width: 0, height: 10)
-//        getGpsAddressView.layer.shouldRasterize = true
-//    }
+    //    private func configureDecoration() {
+    //        getGpsAddressView.layer.shadowColor = UIColor.systemGray.cgColor
+    //        getGpsAddressView.layer.cornerRadius = 16
+    //        getGpsAddressView.layer.shadowOpacity = 0.1
+    //        getGpsAddressView.layer.shadowRadius = 10
+    //        getGpsAddressView.layer.shadowOffset = .init(width: 0, height: 10)
+    //        getGpsAddressView.layer.shouldRasterize = true
+    //    }
 }
 
 
@@ -211,9 +229,9 @@ extension GetGpsAddressVC: MKMapViewDelegate {
         } else {
             // アノテーションを画像にする
             let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-//            annotationView.image = UIImage(named: "jinrikisya_man")
+            //            annotationView.image = UIImage(named: "jinrikisya_man")
             // figure.waveのサイズを大きくしたい
-            annotationView.image = UIImage(systemName: "figure.wave")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 30, weight: .bold))
+            annotationView.image = UIImage(systemName: "house")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 30, weight: .bold))
             annotationView.canShowCallout = true
             return annotationView
         }
@@ -237,37 +255,37 @@ extension GetGpsAddressVC: MKMapViewDelegate {
 
 // 2点間の距離を測定
 extension CLLocationCoordinate2D {
-//    func distanceFromHome(
-//        to targetLoacation: CLLocationCoordinate2D) -> CLLocationDistance {
-//
-//        let location1 = CLLocation(latitude: latitude, longitude: longitude)
-//        let location2 = CLLocation(latitude: targetLoacation.latitude, longitude: targetLoacation.longitude)
-//        return location1.distance(from: location2)
-//    }
+    //    func distanceFromHome(
+    //        to targetLoacation: CLLocationCoordinate2D) -> CLLocationDistance {
+    //
+    //        let location1 = CLLocation(latitude: latitude, longitude: longitude)
+    //        let location2 = CLLocation(latitude: targetLoacation.latitude, longitude: targetLoacation.longitude)
+    //        return location1.distance(from: location2)
+    //    }
     
     // 距離をStringで表示
-//    func distanceTextFromHome(to targetLocation: CLLocationCoordinate2D) -> String {
-//
-//        let rawDistance = distanceFromHome(to: targetLocation)
-//        print("distanceTextFromHome. rawDistance", rawDistance)
-//
-//        // 1km未満は四捨五入で10m単位
-//        if rawDistance < 1000 {
-//            let roundedDistance = (rawDistance / 10).rounded() * 10
-//            return "\(Int(roundedDistance))m"
-//        }
-//        // 1km以上は四捨五入で0.1km単位
-//        let roundedDistance = (rawDistance / 100).rounded() * 100
-//        return "\(roundedDistance / 1000)km"
-//    }
+    //    func distanceTextFromHome(to targetLocation: CLLocationCoordinate2D) -> String {
+    //
+    //        let rawDistance = distanceFromHome(to: targetLocation)
+    //        print("distanceTextFromHome. rawDistance", rawDistance)
+    //
+    //        // 1km未満は四捨五入で10m単位
+    //        if rawDistance < 1000 {
+    //            let roundedDistance = (rawDistance / 10).rounded() * 10
+    //            return "\(Int(roundedDistance))m"
+    //        }
+    //        // 1km以上は四捨五入で0.1km単位
+    //        let roundedDistance = (rawDistance / 100).rounded() * 100
+    //        return "\(roundedDistance / 1000)km"
+    //    }
 }
 
 extension GetGpsAddressVC: GetGeocoderDelegate {
     func getAddressFromCurrentPlace() {
         getCurrentLocation()
-//        swipedActionLabel.text = "取得完了しました"
-//        setAnnotation(location: myHomeLocation)
-//        setAnnotation(location: CLLocationCoordinate2D(latitude: geoCoderLatitude, longitude: geoCoderLongitude))
+        //        swipedActionLabel.text = "取得完了しました"
+        //        setAnnotation(location: myHomeLocation)
+        //        setAnnotation(location: CLLocationCoordinate2D(latitude: geoCoderLatitude, longitude: geoCoderLongitude))
         print("geoCoderLatitude:", geoCoderLatitude)
         print("geoCoderLongitude:", geoCoderLongitude)
     }
