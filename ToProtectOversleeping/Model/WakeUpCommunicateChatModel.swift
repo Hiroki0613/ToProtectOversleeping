0f61be86eb9283bcc68e8d9cd9db64bb25bf75d4
Hiroki Kondo <kondo.matr02@gmail.com>
2021-05-28 23:55:25 +0900

    コメントの削除

--- ToProtectOversleeping/Model/WakeUpCommunicateChatModel.swift
+++ ToProtectOversleeping/Model/WakeUpCommunicateChatModel.swift
@@ -16,11 +16,6 @@ struct Sender: SenderType {
     var displayName: String
 }
 
-//struct MockUser: SenderType, Equatable {
-//    var senderId: String
-//    var displayName: String
-//}
-
 struct RoomNameModel {
     var roomName: String
 }
@@ -31,33 +26,20 @@ struct Message: MessageType {
     var messageId: String
     var sentDate: Date
     var kind: MessageKind
-    
-    // 必須では無い
-//    var userImagePath: String
-//    var date:TimeInterval
-//    var messageImageString:String
-    
-    
-//    private init(kind: MessageKind, user: Sender, messageId: String, date: Date) {
-//        self.kind = kind
-//        self.user = user
-//        self.messageId = messageId
-//        self.sentDate = date
-//    }
-//    
-//    init(text: String, user: Sender, messageId: String, date: Date) {
-//        self.init(kind: .text(text), user: user, messageId: messageId, date: date)
-//    }
 }
 
 struct UserDataModel {
     var uid = String()
     var name = String()
+    
+    init(uid: String, name: String) {
+        self.uid = uid
+        self.name = name
+    }
 }
 
 
 class SendDBModel {
-    
     // 一旦ここでaddSnapをさせる
     let db = Firestore.firestore()
     
@@ -65,15 +47,6 @@ class SendDBModel {
     var toID: String = ""
     var text: String = ""
     var displayName: String = ""
-//    var imageUrlString: String = ""
-    
-//    func sendMessage(senderID:String,text: String, user: String, messageID: String, date: Date) {
-//        self.db.collection("Users").document(senderID).collection("chat").document(messageID).setData(["text": text as Any, "user": currentUser,"messageID": messageID ,"date": Date().timeIntervalSince1970]
-//        )
-//
-////        self.db.collection("Users").document("7654321").collection("chat").document(messageID).setData(["text": text as Any, "user": otherUser as Any, "messageID": messageID,"date": Date().timeIntervalSince1970]
-////        )
-//    }
     
     func sendMessage(senderID: String, toID: String, text: String, displayName: String) {
         self.db.collection("Chats").document(senderID).collection("talk").document(toID).setData([
